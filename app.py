4a0eebdd2d4c00741896dc113024554f9dedfdf0
pgb2409 <bateriapartituras@gmail.com>
2025-10-04 11:07:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, send_file
+from flask_cors import CORS
 from audio_to_musicxml import convertir_mp3_a_musicxml
 import os
 
 app = Flask(__name__)
-UPLOAD_FOLDER = "cargas"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+CORS(app)
 
 @app.route("/convertir", methods=["POST"])
 def convertir():
@@ -12,10 +12,8 @@ def convertir():
     if not archivo:
         return "No se recibió archivo", 400
 
-    ruta_mp3 = os.path.join(UPLOAD_FOLDER, "temp.mp3")
-    ruta_xml = os.path.join(UPLOAD_FOLDER, "partitura.musicxml")
-
+    ruta_mp3 = os.path.join("uploads", "entrada.mp3")
     archivo.save(ruta_mp3)
-    convertir_mp3_a_musicxml(ruta_mp3, ruta_xml)
 
-    return send_file(ruta_xml, as_attachment=True)
+    ruta_xml = convertir_mp3_a_musicxml(ruta_mp3)
+    return send_file(ruta_xml, mimetype="application/vnd.recordare.musicxml+xml")
