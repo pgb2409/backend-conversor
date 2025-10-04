19a08121001c155950baf68d0ff2f03ad18b9bdf
pgb2409 <bateriapartituras@gmail.com>
2025-10-04 02:27:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,29 @@ CORS(app)
 
 @app.route('/convert', methods=['POST'])
 def convert():
+    # Recibe el archivo MP3
     file = request.files['file']
     filename = file.filename
     file.save(filename)
 
-    # Simulación de conversión
+    # Simulación de conversión a MusicXML
     output_file = "resultado.musicxml"
     with open(output_file, "w") as f:
-        f.write("<score-partwise version='3.1'><part><measure><note><unpitched/></note></measure></part></score-partwise>")
+        f.write("""
+        <score-partwise version="3.1">
+            <part>
+                <measure>
+                    <note>
+                        <unpitched/>
+                    </note>
+                </measure>
+            </part>
+        </score-partwise>
+        """)
 
+    # Devuelve el archivo convertido
     return send_file(output_file, as_attachment=True)
 
+# Render necesita que el servidor escuche en 0.0.0.0 y puerto 10000
 if __name__ == '__main__':
-app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=10000)
