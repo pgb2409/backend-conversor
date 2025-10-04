948c4456508b1247d0da4ab178dc9360c4fb0d31
pgb2409 <bateriapartituras@gmail.com>
2025-10-04 09:41:13 +0200

    Update audio_to_musicxml.py

--- audio_to_musicxml.py
+++ audio_to_musicxml.py
@@ -1,35 +1,22 @@
-import sys
-import os
 import librosa
-import music21
-import numpy as np
-import soundfile as sf
+from music21 import stream, note, tempo, meter
 
-def convert_to_musicxml(audio_path):
-    base_name = os.path.splitext(os.path.basename(audio_path))[0]
-    output_path = os.path.join('outputs', base_name + '.musicxml')
+def convertir_mp3_a_musicxml(ruta_mp3, ruta_salida):
+    y, sr = librosa.load(ruta_mp3)
+    onset_frames = librosa.onset.onset_detect(y=y, sr=sr)
+    onset_times = librosa.frames_to_time(onset_frames, sr=sr)
 
-    y, sr = librosa.load(audio_path, sr=None)
-    tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
-    beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+    bpm = librosa.beat.tempo(y=y, sr=sr)[0]
 
-    score = music21.stream.Score()
-    part = music21.stream.Part()
-    part.append(music21.tempo.MetronomeMark(number=int(tempo)))
+    partitura = stream.Score()
+    parte = stream.Part()
+    parte.append(tempo.MetronomeMark(number=bpm))
+    parte.append(meter.TimeSignature("4/4"))
 
-    for t in beat_times:
-        dur = music21.duration.Duration(1)
-        note = music21.note.Note('C4', duration=dur)
-        part.append(note)
+    for t in onset_times:
+        n = note.Note("C4", quarterLength=0.25)
+        n.offset = t
+        parte.append(n)
 
-    score.append(part)
-    score.write('musicxml', fp=output_path)
-    print(f'Archivo generado: {output_path}')
-
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('Uso: python audio_to_musicxml.py archivo.mp3')
-        sys.exit(1)
-
-    audio_file = sys.argv[1]
-    convert_to_musicxml(audio_file)
+    partitura.append(parte)
+    partitura.write("musicxml", fp=ruta_salida)
